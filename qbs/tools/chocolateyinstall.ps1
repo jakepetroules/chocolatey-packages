44fda4e0a422efda5ed0b81ee343ae4d97ae7aee
Jake Petroules <jake.petroules@petroules.com>
2015-07-10 13:22:52 -0700

    Add qbs package.

---
+++ qbs/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+
+$packageName = 'qbs'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://download.qt.io/official_releases/qbs/1.4.1/qbs-windows-x86-1.4.1.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+}
+
+Install-ChocolateyZipPackage @packageArgs
