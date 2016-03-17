1ecc366f1e748e0489a34fea4d96467f0bb53020
Jake Petroules <jake.petroules@petroules.com>
2016-03-16 21:30:04 -0700

    Update qbs package to 1.4.5.

--- qbs/tools/chocolateyinstall.ps1
+++ qbs/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 
 $packageName = 'qbs'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://download.qt.io/official_releases/qbs/1.4.4/qbs-windows-x86-1.4.4.zip'
+$url = 'https://download.qt.io/official_releases/qbs/1.4.5/qbs-windows-x86-1.4.5.zip'
 
 $packageArgs = @{
   packageName   = $packageName
