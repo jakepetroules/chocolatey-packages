605cec49958b74bc9ddc8412d8eb0d369ca74670
Jake Petroules <jake.petroules@petroules.com>
2016-07-08 23:16:06 -0700

    Update jom package to 1.1.1

--- jom/tools/chocolateyinstall.ps1
+++ jom/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName = 'jom'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://download.qt.io/official_releases/jom/jom_1_1_0.zip'
+$url = 'https://download.qt.io/official_releases/jom/jom_1_1_1.zip'
 
 $packageArgs = @{
   packageName   = $packageName
