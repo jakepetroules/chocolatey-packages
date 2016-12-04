99da11135c852d57dd3ddc5cf7fc8a86e8ace22c
Jake Petroules <jake.petroules@petroules.com>
2016-12-03 22:25:47 -0800

    Clean up qbs install script and add checksum

--- qbs/tools/chocolateyinstall.ps1
+++ qbs/tools/chocolateyinstall.ps1
@@ -1,14 +1,8 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop'
 
-
-$packageName = 'qbs'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://download.qt.io/official_releases/qbs/1.6.1/qbs-windows-x86-1.6.1.zip'
-
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  url           = $url
-}
-
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyZipPackage `
+    -PackageName 'qbs' `
+    -Url 'https://download.qt.io/official_releases/qbs/1.6.1/qbs-windows-x86-1.6.1.zip' `
+    -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
+    -Checksum '51FA473BFFDEBFFB68F2A97DCE1FA124A807DB46DA7969675D403E5E0906C9D2D6563EF50309FBE259EC37141B01ADBCEA65EA886D8CDF82615D79306384AD70' `
+    -ChecksumType 'sha512'
