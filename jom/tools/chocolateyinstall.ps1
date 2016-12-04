5d0d3015b4814b284cdbccc2c8db279d8ffe1900
Jake Petroules <jake.petroules@petroules.com>
2016-12-03 22:14:32 -0800

    Clean up jom install script and add checksum

--- jom/tools/chocolateyinstall.ps1
+++ jom/tools/chocolateyinstall.ps1
@@ -1,14 +1,8 @@
-$ErrorActionPreference = 'Stop'; # stop on all errors
+$ErrorActionPreference = 'Stop'
 
-
-$packageName = 'jom'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://download.qt.io/official_releases/jom/jom_1_1_2.zip'
-
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  url           = $url
-}
-
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyZipPackage `
+    -PackageName 'jom' `
+    -Url 'https://download.qt.io/official_releases/jom/jom_1_1_2.zip' `
+    -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
+    -Checksum '830CD94ED6518FBE4604A0F5A3322671B4674B87D25A71349C745500D38E85C0FAC4F6995242FC5521EB048E3966BB5EC2A96A06B041343ED8DA9BBA78124F34' `
+    -ChecksumType 'sha512'
