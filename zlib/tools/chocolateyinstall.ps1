509cca912a3b593d8d196155103e05a83576e576
Jake Petroules <jake.petroules@petroules.com>
2016-12-13 16:11:10 -0800

    Add zlib package

---
+++ zlib/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'zlib' `
+    -Url 'http://zlib.net/zlib128-dll.zip' `
+    -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
+    -Checksum '8fd8dbfb5b01386209872b62e401e639' `
+    -ChecksumType 'md5'
+
+Install-ChocolateyPath `
+    -PathToInstall "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
