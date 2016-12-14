0de179fe8eedcf9f59a0e8fc3c22c5edae566afd
Jake Petroules <jake.petroules@petroules.com>
2016-12-13 16:12:33 -0800

    Add libxslt package

---
+++ libxslt/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'libxslt' `
+    -Url 'http://xmlsoft.org/sources/win32/libxslt-1.1.26.win32.zip' `
+    -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
+    -Checksum 'AE09C820D592DE1504829ED63627F46A4759CD799E625B6332209BC5FF72542312CABE2B6B4F0A68D24C47939584DC63DF32DA021ADC73967AB6E747EBFF99D4' `
+    -ChecksumType 'sha512'
