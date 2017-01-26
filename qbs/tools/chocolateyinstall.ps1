51181541f9b9e4ff3e28d512f1ac5b91950f4186
Jake Petroules <jake.petroules@petroules.com>
2017-01-26 15:58:47 -0800

    Update qbs package to 1.7.1

--- qbs/tools/chocolateyinstall.ps1
+++ qbs/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 Install-ChocolateyZipPackage `
     -PackageName 'qbs' `
-    -Url 'https://download.qt.io/official_releases/qbs/1.7.0/qbs-windows-x86-1.7.0.zip' `
+    -Url 'https://download.qt.io/official_releases/qbs/1.7.1/qbs-windows-x86-1.7.1.zip' `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
-    -Url64bit 'https://download.qt.io/official_releases/qbs/1.7.0/qbs-windows-x86_64-1.7.0.zip' `
-    -Checksum '7756ac0a6dbaf67f9d21bb440dfc0b63e53cf8372387dd81d93c1db6ee2f1dfc46d7f4d54da8cda398d7e5951f9d31759dd4491fee9d04a1003e4084dace3ac8' `
+    -Url64bit 'https://download.qt.io/official_releases/qbs/1.7.1/qbs-windows-x86_64-1.7.1.zip' `
+    -Checksum 'e55c7fa50e24fe34d63183d688134b782baa610eb0a2eb7347c54cd016168cf4357b6c19ee223b36b3f32cef30de0ce1f6ce11f9319ad8bce563879b5d197325' `
     -ChecksumType 'sha512' `
-    -Checksum64 '1081c35c9a73c980d11f9535363b1b375e3db1c596cf998dde036741e32946a8c9eaed29bb0ae97864bc87e334461551297779cc29d3ddf2fe19d83a94036a47' `
+    -Checksum64 'e7e37ae9e726249743f693e97faa3557144e583f5bfd87c030d212dfd24f6639b72d1600846617be3300fea60b8f95e4150c885bbcc6f8155c79ac8013d7232c' `
     -ChecksumType64 'sha512'
