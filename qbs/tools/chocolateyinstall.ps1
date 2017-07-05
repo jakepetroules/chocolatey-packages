0545a298538034cc74b4c4bc088b2eed49975af9
Jake Petroules <jake.petroules@petroules.com>
2017-07-04 20:40:58 -0700

    Update qbs package to 1.8.1

--- qbs/tools/chocolateyinstall.ps1
+++ qbs/tools/chocolateyinstall.ps1
@@ -1,20 +1,25 @@
-﻿$ErrorActionPreference = 'Stop'
+﻿$qbsVersion = '1.8.1'
+
+$ErrorActionPreference = 'Stop'
+
+$qbsBaseUrl = "https://download.qt.io/official_releases/qbs/$qbsVersion"
+$checksumType = 'md5'
+$checksums = @{}
+ForEach ($line in (New-Object Net.WebClient).DownloadString("$qbsBaseUrl/${checksumType}sums.txt").Split(`
+        "`n", [System.StringSplitOptions]::RemoveEmptyEntries)) {
+    $items = $line.Split(" ", [System.StringSplitOptions]::RemoveEmptyEntries)
+    $checksums.Add($items[1], $items[0])
+}
+
+$qbs32 = "qbs-windows-x86-$qbsVersion.zip"
+$qbs64 = "qbs-windows-x86_64-$qbsVersion.zip"
 
 Install-ChocolateyZipPackage `
     -PackageName 'qbs' `
-    -Url 'https://download.qt.io/official_releases/qbs/1.8.0/qbs-windows-x86-1.8.0.zip' `
+    -Url "$qbsBaseUrl/$qbs32" `
     -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
-    -Url64bit 'https://download.qt.io/official_releases/qbs/1.8.0/qbs-windows-x86_64-1.8.0.zip' `
-    -Checksum 'a968f3128fe25cb38859b7cee7023102554ce2745b9626b563d30a9fc97abfe412fb4e38ae0eefcee3a19a9a11afe990ce6e8a4bec06a988353d29477d740bb9' `
-    -ChecksumType 'sha512' `
-    -Checksum64 'a35168ea5264c332044b5e8ee59090a3bb4c45247b17254d07b18de5ae51ef4984cf574ec1d0833669f2e6a3fbfe2ca0facb8c20e9d3da780aeb7330974c4633' `
-    -ChecksumType64 'sha512'
-
-# Workaround until the next release which includes https://codereview.qt-project.org/#/c/195815/
-If (Test-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\qbs-windows-x86_64-1.8.0") {
-    Copy-Item "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\qbs-windows-x86_64-1.8.0\bin\*.dll" `
-        "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\qbs-windows-x86_64-1.8.0\libexec\qbs"
-} Else {
-    Copy-Item "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\qbs-windows-x86-1.8.0\bin\*.dll" `
-        "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\qbs-windows-x86-1.8.0\libexec\qbs"
-}
+    -Url64bit "$qbsBaseUrl/$qbs64" `
+    -Checksum $checksums[$qbs32] `
+    -ChecksumType $checksumType `
+    -Checksum64 $checksums[$qbs64] `
+    -ChecksumType64 $checksumType
